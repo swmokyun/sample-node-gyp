53df600c87e34ccc82a280025fab541da94dc839
syun <syun@mokyn.com>
2021-12-10 14:23:22 +0900

    add sample

--- src/native_ext.cc
+++ src/native_ext.cc
@@ -16,15 +16,25 @@ void Method(const v8::FunctionCallbackInfo<v8::Value> &args)
 
 void Method2(const v8::FunctionCallbackInfo<v8::Value> &args)
 {
+    if (args.Length() != 2)
+    {
+        args.GetReturnValue().Set(-1);
+        return;
+    }
+
     v8::Isolate *isolate = args.GetIsolate();
     v8::HandleScope scope(isolate);
-    args.GetReturnValue().Set(12.34);
+    v8::Local<v8::Context> context = args.GetIsolate()->GetCurrentContext();
+
+    double p1 = args[0]->NumberValue(context).FromJust();
+    double p2 = args[1]->NumberValue(context).FromJust();
+    args.GetReturnValue().Set(p1 + p2);
 }
 
 void init(v8::Local<v8::Object> target)
 {
     NODE_SET_METHOD(target, "hello", Method);
-    NODE_SET_METHOD(target, "hello2", Method2);
+    NODE_SET_METHOD(target, "add", Method2);
 }
 
 NODE_MODULE(binding, init);
--- src/test.ts
+++ src/test.ts
@@ -3,5 +3,8 @@ var addon = require("./build/Release/native_ext.node")
 const ret: string = addon.hello()
 console.log(ret) // Hello World!
 
-const ret2: number = addon.hello2()
-console.log(ret2) // 12.34
+const ret2_1: number = addon.add()
+console.log(ret2_1) // -1
+
+const ret2_2: number = addon.add(1.1, 1.2)
+console.log(ret2_2) // 2.3
